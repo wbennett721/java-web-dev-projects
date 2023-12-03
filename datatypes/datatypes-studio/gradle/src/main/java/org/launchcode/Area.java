3d03e7e32d137b4924a73ac6cafc9a539749bb07
wbennett721 <142439301+wbennett721@users.noreply.github.com>
2023-12-03 17:01:54 -0600

    StudioClass1

---
+++ datatypes/datatypes-studio/gradle/src/main/java/org/launchcode/Area.java
@@ -0,0 +1,18 @@
+package org.launchcode;
+
+import java.util.Scanner;
+public class Area {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please enter a radius: ");
+        double radius = input.nextDouble();
+        double PI = 3.14;
+        double area = PI * radius * radius;
+        System.out.println("The area of a circle with a radius of " + radius + " is: " + area);
+
+        input.close();
+    }
+}
