6c691653fa22f78c81bd3bd696a4c2668bc29dce
wbennett721 <142439301+wbennett721@users.noreply.github.com>
2023-12-06 23:21:19 -0600

    ExerciseChapter4

--- classes-part-one/exercises/src/main/java/StudentPractice.java
+++ classes-part-one/exercises/src/main/java/StudentPractice.java
@@ -1,7 +1,7 @@
 public class StudentPractice {
     public static void main(String[] args){
-        //insantiate your Student class below
-        Student student = new Student();
-
+        //instantiate your Student class below
+        Student student = new Student("Whitney", 12345, 1, 4.0);
+        System.out.println(student);
     }
 }
