e9f87b2c7eb947a099d32fcfd9d08af500d6134d
wbennett721 <142439301+wbennett721@users.noreply.github.com>
2023-12-06 23:12:56 -0600

    ExerciseChapter4

--- classes-part-one/exercises/src/main/java/Student.java
+++ classes-part-one/exercises/src/main/java/Student.java
@@ -6,5 +6,42 @@ public class Student {
 
         // Drop your getters and setters below for the Student class.
         // To instantiate the Student class, add your code to the main in the file, SchoolPractice.
+        public void setName(String name) {
+                this.name = name;
+        }
+
+        public void setStudentId(int studentId) {
+                this.studentId = studentId;
+        }
+
+        public void setGpa(double gpa) {
+                this.gpa = gpa;
+        }
+
+        private void setNumberOfCredits(int numberOfCredits) {
+                this.numberOfCredits = numberOfCredits;
+        }
+
+        public String getName() {
+                return name;
+        }
+
+        public int getStudentId() {
+                return studentId;
+        }
+
+        public int getNumberOfCredits() {
+                return numberOfCredits;
+        }
+
+        public double getGpa() {
+                return gpa;
+        }
 
 }
+public class Course {
+        private String topic;
+        private Teacher instructor;
+        private ArrayList<Student> enrolledStudents;
+}
+
---
+++ classes-part-one/exercises/src/main/java/Teacher.java
@@ -0,0 +1,38 @@
+public class Teacher {
+    private String firstName;
+    private String lastName;
+    private String subject;
+    private int yearsTeaching;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public int getYearsTeaching() {
+        return yearsTeaching;
+    }
+
+    public void setYearsTeaching(int yearsTeaching) {
+        this.yearsTeaching = yearsTeaching;
+    }
+}
