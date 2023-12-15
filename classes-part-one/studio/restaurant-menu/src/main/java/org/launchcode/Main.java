7040fd23dd669cff6f8920c8c78aa0a2e6971bb5
wbennett721 <142439301+wbennett721@users.noreply.github.com>
2023-12-14 18:34:56 -0600

    Studio

--- classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Main.java
+++ classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Main.java
@@ -4,5 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        MenuItem item1 = new MenuItem("Cheese Pizza", "A slice of pizza with cheese only",  7.99,  "appetizer");
+        MenuItem item2 = new MenuItem( "Honey Hot Wings",  "6 pieces of chicken dip in honey hot sauce",  10.99,  "appetizer");
+        MenuItem item3 = new MenuItem( "Steak and Baked Potato", "6 oz sirloin with a baked potato with cheese",  17.99,  "main course");
+        MenuItem item4 = new MenuItem( "Strawberry Cheese Cake", "A New York slice of cheese cake with strawberry toppings",  7.99,  "dessert");
+        MenuItem item5 = new MenuItem( "Strawberry Cake", "One slice of strawberry cake with real strawberry toppings",  8.99,  "dessert");
+        //System.out.println(item1);
     }
 }
--- classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/MenuItem.java
+++ classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/MenuItem.java
@@ -1,6 +1,7 @@
 package org.launchcode;
 
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 public class MenuItem {
 
@@ -8,49 +9,71 @@ public class MenuItem {
     private String description;
     private double price;
     private String category;
-    private final Date dateAdded;
+    private final LocalDate dateAdded;
 
     public MenuItem(String name, String description, double price, String category) {
         this.name = name;
         this.description = description;
         this.price = price;
         this.category = category;
-        this.dateAdded = new Date();
+        this.dateAdded = LocalDate.now();
     }
 
     public String getName() {
+
         return name;
     }
 
     public void setName(String name) {
+
         this.name = name;
     }
 
     public String getDescription() {
+
         return description;
     }
 
     public void setDescription(String description) {
+
         this.description = description;
     }
 
     public double getPrice() {
+
         return price;
     }
 
     public void setPrice(double price) {
+
         this.price = price;
     }
 
     public String getCategory() {
+
         return category;
     }
 
     public void setCategory(String category) {
+
         this.category = category;
     }
 
-    public Date getDateAdded() {
+    public LocalDate getDateAdded() {
+
         return dateAdded;
     }
+
+    boolean isNew() {
+        LocalDate today = LocalDate.now();
+        double daysBetween = getDateAdded().until(today, ChronoUnit.DAYS);
+        return daysBetween < 30;
+    }
+
+    @Override
+    public String toString() {
+        String newText = isNew() ? " - NEW!" : "";
+        return name + newText + "\n" + description + " | $" + price;
+    }
+
 }
