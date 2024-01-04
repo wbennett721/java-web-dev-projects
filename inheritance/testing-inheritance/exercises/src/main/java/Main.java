1b7b2bc3f446730ea61861f9110ebe9908969088
wbennett721 <142439301+wbennett721@users.noreply.github.com>
2024-01-03 19:05:53 -0600

    ExerciseCh8

---
+++ inheritance/testing-inheritance/exercises/src/main/java/Main.java
@@ -0,0 +1,4 @@
+public class Main { HouseCat garfield = new HouseCat("Garfield", 12);
+ garfield.eat();
+System.out.println(garfield.isTired());
+}
--- interfaces/exercises/ice-cream-exercises/src/main/java/org/launchcode/Main.java
+++ interfaces/exercises/ice-cream-exercises/src/main/java/org/launchcode/Main.java
@@ -1,17 +1,20 @@
 package org.launchcode;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args) {
         Case menu = new Case();
         ArrayList<Flavor> flavors = menu.getFlavors();
         ArrayList<Cone> cones = menu.getCones();
+        Comparator comparator = new FlavorComparator();
+        flavors.sort(comparator);
 
+    }
         // TODO: Use a Comparator class to sort the 'flavors' array alphabetically by the 'name' field.
 
         // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost' field.
 
         // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
     }
-}
